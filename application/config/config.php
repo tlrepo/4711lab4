172210c267a9947a5a789bbf606778a083a7bc84
Dpark50 <daniel51_1994@hotmail.com>
2017-09-28 16:52:31 -0700

    Resolve conflicts
    Signed-off-by: Daniel <daniel51_1994@hotmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -6,7 +6,7 @@ $config['menu_choices'] = array(
     'menudata' => array(
         array('name' => 'Alpha', 'link' => '/able'),
         array('name' => 'Bravo', 'link' => '/bravo'),
-        array('name' => 'Charlie', 'link' => '#'),
+        array('name' => 'Charlie', 'link' => '/charlie/brown'),
         array('name' => 'Delta', 'link' => '#'),
         array('name' => 'Echo', 'link' => '#'),
         array('name' => 'Foxtrot', 'link' => '#'),
