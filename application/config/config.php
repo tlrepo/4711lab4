dc10d20fb2e9758e00d9a93bfb37b25d80011903
Dpark50 <daniel51_1994@hotmail.com>
2017-09-28 17:39:51 -0700

    closes #11
    Signed-off-by: Daniel <daniel51_1994@hotmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@ $config['menu_choices'] = array(
         array('name' => 'Hotel', 'link' => 'bananas/rule'),
         array('name' => 'India', 'link' => '#'),
         array('name' => 'Juliet', 'link' => '#'),
-        array('name' => 'Kilo', 'link' => '#'),
+        array('name' => 'Kilo', 'link' => '/show/2'),
         )
     );
 
